d94d3d22fa75bf4a8a18c532b7ab97f2fcbe26be
BabyScylla69 <alvarosilvalobos99@gmail.com>
2024-11-27 19:47:08 +0100

    login fix

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,18 +1,38 @@
 import { useNavigate } from "react-router-dom"
+import { useUserContext } from "../providers/UserProvider"
+import { useState } from "react"
+import { login } from "../services/api"
 
 const LogIn = () => {
    const navigate = useNavigate()
+   const {setUser} = useUserContext()
+   const [u, setU] = useState({username: '', password: ''})
 
    function goBack() {
       navigate("/")
    }
-   function handleClick() {
+
+   function register() {
       navigate("/register")
    }
+
+   const handleClick = async () => {
+      const response = await login(u)
+      setUser(response)
+      navigate("/home")
+   }
+
    return(
       <div>
          <button type="button" onClick={goBack}>Back</button>
-         <button type="button" onClick={handleClick}>register</button>
+         <br />   
+         <br />   
+         <input type="text" placeholder="username" onChange={(e) => setU({...u, username: e.target.value})} />
+         <input type="password" placeholder="password" onChange={(e) => setU({...u, password: e.target.value})} />
+         <button onClick={handleClick}>Log in</button> 
+         <br />
+         <br />   
+         <button type="button" onClick={register}>register</button>
       </div>
    )
 }
---
+++ src/services/api.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const i = axios.create({
+  baseURL: 'http://localhost:8080'
+})
+
+export const login = async ({username, password}) =>{
+
+  const token = btoa(username + ':' + password);
+  const response =  await i.post('/signin', {},
+  {
+    headers: {  
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + token
+    }
+  }
+  );
+  setAuth(token);
+  return response.data;
+}
+
+export const getUsers = async () =>{
+  const response = await i.get('/users');
+    return response.data;
+}
+
+// Function to create a user
+export const createUser = async (user) => {
+  try {
+    const response = await i.post('/signup', user); // Adjust endpoint as necessary
+    return response.data; // Return the response data
+  } catch (error) {
+    // Handle error appropriately
+    console.error('Error creating user:', error.response?.data || error.message);
+    throw error;
+  }
+};
+
+const setAuth = async (token) => {
+  i.defaults.headers.common.Authorization = `basic ${token}`;
+};
