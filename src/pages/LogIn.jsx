46464a5e87f53b5b1fa796d44acd1b46605a18e6
Jordi <jordiigarciatorres@gmail.com>
2024-11-28 17:07:56 +0100

    css login, register and home page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,19 +1,46 @@
-import { useNavigate } from "react-router-dom"
+import NavBar from "../components/NavBar"
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import { useNavigate } from "react-router-dom";
 
 const LogIn = () => {
-   const navigate = useNavigate()
+   const navigate = useNavigate();
+   const handleClick = () => {
+      navigate("/home");
+   }
 
-   function goBack() {
-      navigate("/")
-   }
-   function handleClick() {
-      navigate("/register")
-   }
    return(
-      <div>
-         <button type="button" onClick={goBack}>Back</button>
-         <button type="button" onClick={handleClick}>register</button>
-      </div>
+      <>
+      <NavBar/>
+      <Container fluid className="d-flex justify-content-center align-items-center vh-100">
+            <Form className="p-3 border rounded" style={{ width: "100%", maxWidth: "700px"}}>
+               <Row className="mb-3">
+                  <Form.Group as={Col} controlId="formGridUsername">
+                     <Form.Label>Username</Form.Label>
+                     <Form.Control type="text" placeholder="Enter username" />
+                  </Form.Group>
+               </Row>
+
+               <Row className="mb-3">               
+                  <Form.Group as={Col} controlId="formGridPassword">
+                     <Form.Label>Password</Form.Label>
+                     <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+               </Row>
+
+               <Row>
+                <Col md={{ span: 6, offset: 10 }}>
+                  <Button variant="primary" type="submit" onClick={handleClick}>
+                     Log In
+                  </Button>
+                </Col>
+               </Row>
+            </Form>
+         </Container>
+         </>
    )
 }
 
