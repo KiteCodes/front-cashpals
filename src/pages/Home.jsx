ae25797f5d432b0c3f1fcd1ccb8626bd89b9f338
Jordi <jordiigarciatorres@gmail.com>
2024-12-02 19:01:34 +0100

    add balance into homepage after login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom"
 import LoggedNavBar from "../components/LoggedNavBar"
 import { Container } from "react-bootstrap"
+import ListGroup from "react-bootstrap/ListGroup"
 
 const Home = () => {
    const navigate = useNavigate()
@@ -29,15 +30,59 @@ const Home = () => {
    return (
       <>
          <LoggedNavBar/>
-         <Container fluid className="d-flex justify-content-center align-items-center vh-100">
+         <Container fluid className="d-flex justify-content-center vh-100">
             <Container fluid className="d-flex flex-column justify-content-center align-items-center" >
                <h1>Your Balance</h1>
-               <Container>
-                  Contenido
+               <Container fluid className="d-flex justify-content-center" style={{ width: "100%", maxWidth: "800px"}}>
+                  <Container className="d-flex flex-column">
+                     <p className="align-self-center">Deben</p>
+                     {/*Esto lo hará iterando sobre las transacciones del usuario logueado*/}
+                     <ListGroup>
+                        <ListGroup.Item>
+                        Link 1
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                        Link 2
+                        </ListGroup.Item>
+                        <ListGroup.Item action onClick={()=> goNavigate("/group/1")} style={{color: "red"}}>
+                        This one is a button
+                        </ListGroup.Item>
+                     </ListGroup>
+                  </Container>
+                  <Container className="d-flex flex-column">
+                  <p className="align-self-center">Debo</p>
+                     {/*Esto lo hará iterando sobre las transacciones del usuario logueado*/}
+                     <ListGroup>
+                        <ListGroup.Item>
+                        Link 1
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                        Link 2
+                        </ListGroup.Item>
+                        <ListGroup.Item action onClick={()=> goNavigate("/group/1")}>
+                        This one is a button
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                        Link 1
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                        Link 2
+                        </ListGroup.Item>
+                        <ListGroup.Item action onClick={()=> goNavigate("/group/1")}>
+                        This one is a button
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                        Link 1
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                        Link 2
+                        </ListGroup.Item>
+                        <ListGroup.Item action onClick={()=> goNavigate("/group/1")}>
+                        This one is a button
+                        </ListGroup.Item>
+                     </ListGroup>
+                  </Container>
                </Container>
-            </Container>
-            <Container fluid className="d-flex justify-content-center align-items-center" >
-               <h1>Recent Groups</h1>
             </Container>
          </Container>
       </>
