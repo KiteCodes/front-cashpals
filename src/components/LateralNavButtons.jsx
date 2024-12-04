723237c08fad28cc221e7cdf56c30593cf4ae003
Jordi <jordiigarciatorres@gmail.com>
2024-12-04 18:45:06 +0100

    restore lateralnavbuttons

--- src/components/LateralNavButtons.jsx
+++ src/components/LateralNavButtons.jsx
@@ -2,7 +2,6 @@ import { Button } from 'react-bootstrap';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Tab from 'react-bootstrap/Tab';
 import {useNavigate} from 'react-router-dom';
-import { Row, Col } from 'react-bootstrap';
 
 const LateralNavButtons = () => {
   const navigate = useNavigate()
@@ -12,21 +11,23 @@ const LateralNavButtons = () => {
   }
   return (
     <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
-      <Row>
-        <Col >
-          <ListGroup>
-            <ListGroup.Item action onClick={()=> goNavigate("/home")}>
-              Home
-            </ListGroup.Item>
-            <ListGroup.Item action onClick={()=> goNavigate("/groups")}>
-              Groups
-            </ListGroup.Item>
-            <ListGroup.Item action onClick={()=> goNavigate("/contacts")}>
-              Contacts
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>  
-      </Row>
+      <ListGroup>
+        <ListGroup.Item action href="/home">
+          Home
+        </ListGroup.Item>
+        <ListGroup.Item action href="/groups">
+          Groups
+        </ListGroup.Item>
+        <ListGroup.Item action href="/contacts">
+          Contacts
+        </ListGroup.Item>
+      </ListGroup>
+
+      <Button variant="primary" 
+      onClick={()=> goNavigate("/profile")}
+      style={{marginBottom: '20px'}}>
+        Profile Settings
+      </Button>
     </Tab.Container>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,3 @@
-import {useNavigate} from 'react-router-dom';
 import LoggedNavBar from '../components/LoggedNavBar';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
