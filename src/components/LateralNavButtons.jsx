789107be0fe47a5e08d9b5550c92426c75aa8f0f
Jordi <jordiigarciatorres@gmail.com>
2024-12-04 19:22:30 +0100

    correct lateralnavbuttons

--- src/components/LateralNavButtons.jsx
+++ src/components/LateralNavButtons.jsx
@@ -12,13 +12,13 @@ const LateralNavButtons = () => {
   return (
     <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
       <ListGroup>
-        <ListGroup.Item as={'button'} action onClick={goNavigate("/home")}>
+        <ListGroup.Item action onClick={() => goNavigate("/home")}>
           Home
         </ListGroup.Item>
-        <ListGroup.Item as={'button'} action onClick={goNavigate("/groups")}>
+        <ListGroup.Item action onClick={() => goNavigate("/groups")}>
           Groups
         </ListGroup.Item>
-        <ListGroup.Item as={'button'} action onClick={goNavigate("/contacts")}>
+        <ListGroup.Item action onClick={() => goNavigate("/contacts")}>
           Contacts
         </ListGroup.Item>
       </ListGroup>
