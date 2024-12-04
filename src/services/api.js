763f2bc9744795e5fd9cb49a1f578ae83d00266e
Jordi <jordiigarciatorres@gmail.com>
2024-12-04 18:09:39 +0100

    change api.js url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const i = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'https://back-cashpals.onrender.com'
 })
 
 export const login = async ({username, password}) =>{
