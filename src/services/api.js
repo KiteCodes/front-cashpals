794edbbcf6015f0a1d27c04b4bf21fe32c1c57d1
BabyScylla69 <alvarosilvalobos99@gmail.com>
2024-12-10 19:14:37 +0100

    added contacts logic and endpoints

--- src/services/api.js
+++ src/services/api.js
@@ -45,101 +45,111 @@ export const getUsers = async () => {
 
 export const getUserById = async (id) => {
   const response = i.get('/user/' + id)
-  return response.data
+    return response.data
 }
 
 export const modifyUser = async (id, user) => {
   const response = await i.put('/user/' + id, user)
-  return response.data
+    return response.data
 }
 
 export const deleteUser = async (id) => {
   const response = await i.delete('/user/' + id)
-  return response.data
+    return response.data
 } 
+
+export const getContacts = async (id) => {
+  const response = await i.get('/user/contacts', id)
+    return response.data
+}
+
+export const saveContacts = async (contactList) => {
+  const response = await i.put('/user/contacts', contactList)
+    return response.data
+}
 
 // GROUPS RELATED
 
 export const getGroups = async () => {
   const response = await i.get('/party')
-  return response.data
+    return response.data
 }
 
 export const createGroup = async (group) => {
   const response = await i.post('/party', group)
-  return response.data
+    return response.data
 }
 
 export const getGroupById = async (id) => {
   const response = await i.get('/party/' + id)
-  return response.data
+    return response.data
 }
 
 export const modifyGroupById = async (id, group) => { // delete group es ahora disable group, no se borran
   const response = await i.put('/party/' + id, group)
-  return response.data
+    return response.data
 } 
 
 // EVENT RELATED 
 
 export const getEvents = async () => {
   const response = await i.get('/event')
-  return response.data
+    return response.data
 }
 
 export const createEvent = async (event) => {
   const response = await i.post('/event', event)
-  return response.data
+   return response.data
 } 
 
 export const getEventById = async (id) => {
   const response = await i.get('/event/' + id)
-  return response.data
+   return response.data
 }
 
 export const modifyEvent = async (id, event) => {
   const response = await i.put('/event/' + id, event)
-  return response.data
+    return response.data
 }
 
 export const deleteEvent = async (id) => {
   const response = await i.delete('/event/' + id)
-  return response.data
+    return response.data
 }
 
 // TRANSACTION RELATED 
 
 export const getTransactions = async () => {
   const response = i.get('/transaction')
-  return response.data
+    return response.data
 }
 
 export const createTransaction = async (transaction) => {
   const response = i.post('/transaction', transaction)
-  return response.data
+    return response.data
 }
 
 export const getTransactionById = async (id) => {
   const response = i.get('/transaction/' + id)
-  return response.data
+    return response.data
 }
 
 export const modifyTransaction = async (id, transaction) => {
   const response = i.put('transaction/' + id, transaction)
-  return response.data
+    return response.data
 }
 
 export const deleteTransaction = async (id) => {
   const response = i.delete('/transaction/' + id)
-  return response.data
+    return response.data
 }
 
 export const getTransactionByDebtorId = async (id) => {
   const response = i.get('/transaction/debtor/' + id)
-  return response.data
+    return response.data
 }
 
 export const getTransactionByIndebtedId = async (id) => {
   const response = i.get('/transaction/indebted' + id)
-  return response.data
+    return response.data
 }
